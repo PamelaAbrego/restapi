e53c6b56b08109afb2bb884552eeeb7c840116d7
Pamela Abrego <pamelabregor@gmail.com>
2021-07-30 02:24:39 -0600

    -

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from resources.person_resource import Person
-from resources.contenido import contenido
-from resources.trainers import trainers
+from resource.contenido import contenido
+from resource.trainers import trainers
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Person, "/all_person")
 api.add_resource(contenido, "/contenido/<string:categoria>")
 api.add_resource(trainers, "/trainers/<string:categoria>")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
